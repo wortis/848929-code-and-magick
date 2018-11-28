1f21bb79bdd2845966737941bdfebfb1a58eee4b
wortis <latre9@yandex.ru>
2018-11-28 17:03:13 +0300

    поправил код согласно замечаниям

--- js/setup.js
+++ js/setup.js
@@ -2,58 +2,62 @@
 
 var setup = document.querySelector('.setup');
 var setupSimilar = document.querySelector('.setup-similar');
-var totalSimilarWizards = 4;
-var availableNamesOfWizard = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var availableSurnamesOfWizard = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var availableCoatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var availableEyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+
+showElement(setup);
+showElement(setupSimilar);
+
+rendListWizards(genListWizards(4));
 
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function generateWizardName(names, surnames) {
+function genWizardName(names, surnames) {
   return names[getRandomNumber(0, names.length - 1)] + ' ' + surnames[getRandomNumber(0, surnames.length - 1)];
 }
 
-function generateSimilarWizardData() {
+function genWizardData() {
+  var NamesWizard = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  var SurnamesWizard = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  var CoatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var EyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
   var similarWizard = {};
 
-  similarWizard.name = generateWizardName(availableNamesOfWizard, availableSurnamesOfWizard);
-  similarWizard.coatColor = availableCoatColor[getRandomNumber(0, availableCoatColor.length - 1)];
-  similarWizard.eyesColor = availableEyesColor[getRandomNumber(0, availableEyesColor.length - 1)];
+  similarWizard.name = genWizardName(NamesWizard, SurnamesWizard);
+  similarWizard.coatColors = CoatColors[getRandomNumber(0, CoatColors.length - 1)];
+  similarWizard.eyesColors = EyesColors[getRandomNumber(0, EyesColors.length - 1)];
 
   return similarWizard;
 }
 
-function generateListOfSimilarWizards(totalWizards) {
+function genListWizards(totalWizards) {
   var wizardsList = [];
   for (var i = 0; i < totalWizards; i++) {
-    wizardsList.push(generateSimilarWizardData());
+    wizardsList.push(genWizardData());
   }
   return wizardsList;
 }
 
-function genWizardsList(wizardElement) {
+function genWizardsListElement(wizardElement) {
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
   var similarWizardElement = similarWizardTemplate.cloneNode(true);
   var similarWizardName = similarWizardElement.querySelector('.setup-similar-label');
-  var similarWizardCoatColor = similarWizardElement.querySelector('.wizard-coat');
-  var similarWizardEyesColor = similarWizardElement.querySelector('.wizard-eyes');
+  var similarWizardCoatColors = similarWizardElement.querySelector('.wizard-coat');
+  var similarWizardEyesColors = similarWizardElement.querySelector('.wizard-eyes');
 
   similarWizardName.textContent = wizardElement.name;
-  similarWizardCoatColor.style.fill = wizardElement.coatColor;
-  similarWizardEyesColor.style.fill = wizardElement.eyesColor;
+  similarWizardCoatColors.style.fill = wizardElement.coatColors;
+  similarWizardEyesColors.style.fill = wizardElement.eyesColors;
 
   return similarWizardElement;
 }
 
-function renderListOfSimilarWizards(wizardsList) {
+function rendListWizards(wizardsList) {
   var similarWizardsList = document.querySelector('.setup-similar-list');
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < wizardsList.length; i++) {
-    fragment.appendChild(genWizardsList(wizardsList[i]));
+    fragment.appendChild(genWizardsListElement(wizardsList[i]));
   }
 
   similarWizardsList.appendChild(fragment);
@@ -62,8 +66,3 @@ function renderListOfSimilarWizards(wizardsList) {
 function showElement(element) {
   element.classList.toggle('hidden');
 }
-
-showElement(setup);
-showElement(setupSimilar);
-
-renderListOfSimilarWizards(generateListOfSimilarWizards(totalSimilarWizards));
